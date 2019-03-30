17f715549fcfa32f4bc92248df5afa9df126de72
A_lon <190135250@qq.com>
2019-03-30 17:17:16 +0800

    1

--- temp/main.js
+++ temp/main.js
@@ -1,13 +1,13 @@
 /* var fs = require('fs');
-var data = fs.readFileSync('input.txt');
+var data = fs.readFileSync('temp/input.txt');
 console.log(data.toString());
-console.log("程序结束!");
- */
+console.log("程序结束!"); */
 
-/* var fs = require('fs');
-fs.readFile("input.txt", function (err, data) {
+
+var fs = require('fs');
+fs.readFile("temp/input.txt", function (err, data) {
     if (err)
         return console.error(err);
     console.log(data.toString());
 })
-console.log("程序结束！") */
+console.log("程序结束！");

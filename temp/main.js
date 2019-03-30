b10916e4cb395c9ae4ec7417eba7a3f90c2c46e8
A_lon <190135250@qq.com>
2019-03-30 17:57:29 +0800

    关闭异步调用！

--- temp/main.js
+++ temp/main.js
@@ -1,13 +1,15 @@
-/* var fs = require('fs');
+var fs = require('fs');
 var data = fs.readFileSync('temp/input.txt');
 console.log(data.toString());
-console.log("程序结束!"); */
+console.log("程序结束!");
 
 
+/* 
+console.log("异步调用！");
 var fs = require('fs');
 fs.readFile("temp/input.txt", function (err, data) {
     if (err)
         return console.error(err);
     console.log(data.toString());
 })
-console.log("程序结束！");
+console.log("程序结束！"); */
